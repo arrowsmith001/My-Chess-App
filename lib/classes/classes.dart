e1a86bf947296f1917eded6f58ee654f6606f077
Alexander Arrowsmith <arrowsmithalexander@gmail.com>
2020-08-24 04:32:56 +0100

    changes

--- lib/classes/classes.dart
+++ lib/classes/classes.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:chess_app_example/utils/utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/widgets/image.dart';
 
 enum PieceType{
@@ -34,6 +35,7 @@ class BoardManager{
   }
 
   List<List<Space>> board;
+  bool whitesTurn = true;
 
   void PlacePiece(Piece piece, BoardPosition pos) {
     board[pos.row][pos.col].piece = piece;
@@ -65,10 +67,8 @@ class BoardManager{
         }
       }
 
-  }
+    CalculateValidMoves();
 
-  Piece GetPiece(BoardPosition pos) {
-    return board[pos.row][pos.col].piece;
   }
 
   void ReplacePiece(Piece piece, BoardPosition pos) {
@@ -77,7 +77,152 @@ class BoardManager{
     piece.currentPos = pos;
   }
 
+  Space GetSpace(BoardPosition pos) {
+    try{
+      return board[pos.row][pos.col];
+    }catch(e){
+      return null;
+    }
+  }
+
+  bool IsSpaceOccupiedByColour(BoardPosition pos, PieceColor color){
+    Piece piece = GetSpace(pos).piece;
+    if(piece == null) return false;
+    return piece.color == color;
+
+  }
+
+  bool IsSpaceOccupied(BoardPosition pos){
+    Piece piece = GetSpace(pos).piece;
+    return piece != null;
+  }
+
+  bool IsValidMove(Piece piece, BoardPosition pos) {
+    Space space = board[pos.row][pos.col];
+    return piece.ValidMoves.indexWhere((p) => p.Equals(pos)) != -1;
+  }
+
+  void SwitchTurns(){
+    whitesTurn = !whitesTurn;
+  }
+
+  void CalculateValidMoves() {
+
+    for(int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+
+        Piece piece = board[i][j].piece;
+        if(piece == null) continue;
+
+        piece.ValidMoves = [];
+        if(whitesTurn && piece.color == PieceColor.black) continue;
+        if(!whitesTurn && piece.color == PieceColor.white) continue;
+
+        PieceColor enemyColour = piece.color == PieceColor.white ? PieceColor.black : PieceColor.white;
+
+        switch(piece.type)
+        {
+          case PieceType.pawn:
+
+            int forward = (piece.color == PieceColor.white ? -1 : 1);
+            int startRow = (piece.color == PieceColor.white ? 6 : 1);
+
+            // Space in front
+            BoardPosition positionInFront = BoardPosition(i + forward, j);
+            if(GetSpace(positionInFront)!=null && !IsSpaceOccupied(positionInFront)) piece.ValidMoves.add(positionInFront);
+
+            // 2 spaces if on start row
+            if(i==startRow)
+            {
+              BoardPosition twoSpacesAhead = BoardPosition(i + 2*forward, j);
+              if(!IsSpaceOccupied(twoSpacesAhead)) piece.ValidMoves.add(twoSpacesAhead);
+            }
+
+            // Check for enemies on diagonals
+            BoardPosition frontLeft = BoardPosition(i + forward, j - 1);
+            if(GetSpace(frontLeft) != null)
+            {  if(IsSpaceOccupiedByColour(frontLeft, enemyColour)) piece.ValidMoves.add(frontLeft); }
+
+            BoardPosition frontRight = BoardPosition(i + forward, j + 1);
+            if(GetSpace(frontRight) != null)
+            {  if(IsSpaceOccupiedByColour(frontRight, enemyColour)) piece.ValidMoves.add(frontRight); }
+
+            break;
+          case PieceType.knight:
+
+            List<List<int>> jumps = [[1, 2], [1, -2], [-1, 2],[-1, -2], [2,1], [2,-1], [-2, 1], [-2, -1]];
+            for(List<int> jump in jumps){
+
+              BoardPosition jumpPos = BoardPosition(i + jump[0], j + jump[1]);
+              Space jumpSpace = GetSpace(jumpPos);
+
+              if(jumpSpace == null) continue;
+              if(jumpSpace.piece == null || IsSpaceOccupiedByColour(jumpPos, enemyColour))
+                {
+                  piece.ValidMoves.add(jumpPos);
+                }
+            }
+
+
+            break;
+          case PieceType.bishop:
+
+            List<List<int>> incrs = [[1, 1], [1, -1], [-1, 1],[-1, -1]];
+            BoardPosition pos = BoardPosition(i, j);
+
+            for(List<int> incr in incrs)
+              {
+                int m = 1;
+
+                while(GetSpace(pos.move(incr[0]*m, incr[1]*m)) != null){
+                  BoardPosition newPos = pos.move(incr[0]*m, incr[1]*m);
+                  if(IsSpaceOccupiedByColour(newPos, piece.color))
+                    {
+                      continue;
+                    }
+                  else if(IsSpaceOccupiedByColour(newPos, enemyColour))
+                    {
+                      piece.ValidMoves.add(newPos);
+                      continue;
+                    }
+
+                  piece.ValidMoves.add(newPos);
+                  m++;
+                }
+
+              }
+
+            break;
+          case PieceType.queen:
+
+            break;
+          case PieceType.king:
+
+            break;
+          case PieceType.rook:
+
+            break;
+        }
+
+
+
+
+
+      }
+    }
+  }
+
+  bool DetectPromotion(Piece piece, BoardPosition pos) {
+    if(piece.type != PieceType.pawn) return false;
+    return(pos.row % 7 == 0);
+  }
+
+
 }
+
+//class ChessActions{
+//  static List<BoardPOsitions>[]
+//}
 
 class Space {
   Piece piece = null;
@@ -89,9 +234,12 @@ class Piece{
   PieceColor color;
   BoardPosition currentPos;
   List<BoardPosition> ValidMoves = [];
+  
 }
 
-class Pawn extends Piece{Pawn(PieceColor color, BoardPosition pos) : super(color, pos) {this.type = PieceType.pawn;}}
+class Pawn extends Piece{Pawn(PieceColor color, BoardPosition pos) : super(color, pos) {this.type = PieceType.pawn;
+  bool hasMoved; }
+}
 class Knight extends Piece{Knight(PieceColor color, BoardPosition pos) : super(color, pos){this.type = PieceType.knight;}}
 class Bishop extends Piece{Bishop(PieceColor color, BoardPosition pos) : super(color, pos){this.type = PieceType.bishop;}}
 class King extends Piece{King(PieceColor color, BoardPosition pos) : super(color, pos){this.type = PieceType.king;}}
@@ -104,4 +252,11 @@ class BoardPosition {
 
   int row;
   int col;
+
+  @override
+  bool Equals(BoardPosition pos) {return this.row == pos.row && this.col == pos.col; }
+
+  BoardPosition move(int i, int j) {
+    return BoardPosition(row + i, col + j);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  print('app loaded');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Chess App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        
-        
-        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'My Chess App'),
@@ -37,9 +35,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  BoardPosition hoverHighlight;
+  bool hoverWillAccept;
+
   @override
   void initState() {
-
     widget.bm.Setup();
   }
 
@@ -55,41 +55,96 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
 
         body: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
           itemCount: 64,
           itemBuilder: (BuildContext context, int index) {
-              int i = (index / 8).floor();
-              int j = index % 8;
+            int i = (index / 8).floor();
+            int j = index % 8;
 
-              BoardPosition pos = new BoardPosition(i, j);
+            BoardPosition pos = new BoardPosition(i, j);
+            Space space = widget.bm.GetSpace(pos);
+            Piece piece = space.piece;
+            Image img = getImageOfPiece(piece);
 
-              Piece piece = widget.bm.GetPiece(pos);
-              Image img = getImageOfPiece(piece);
+            // Main content for the space
+            Widget spaceContent = piece == null ? Empty() :
+            Draggable<Piece>(
+                dragAnchor: DragAnchor.pointer,
+                maxSimultaneousDrags: 1,
+                data: piece,
+                //feedbackOffset: Offset.fromDirection(0, 50),
+                feedback: Transform.translate(
+                  offset: Offset(-40, -40),
+                  child: SizedBox(
+                    child: Opacity(child: img, opacity: 0.5,),
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+                childWhenDragging: Container(),
+                child: Align(child: img, alignment: Alignment.center,) //FittedBox(fit: BoxFit.none, child: img),
+            );
 
-              return DragTarget<Piece>(
-                onAccept: (data) {
-                  setState(() {
-                    widget.bm.ReplacePiece(data, pos);
+            // Coloured square which represents the highlight when hovering a piece over a square.
+            Widget highlight = hoverHighlight == null || !hoverHighlight.Equals(pos) ? Empty() :
+              IgnorePointer(
+                child: Opacity(
+                  child: Container(color: hoverWillAccept ? Colors.green : Colors.red),
+                  opacity: 0.3,
+                ));
+
+            return DragTarget<Piece>(
+
+              onAccept: (piece) {
+                setState(() {
+                  widget.bm.ReplacePiece(piece, pos);
+                  hoverHighlight = null;
+                });
+
+                widget.bm.SwitchTurns();
+
+                // Detect promotion opportunities
+                if(widget.bm.DetectPromotion(piece, pos)) {
+                  print('promotion detected!');
+
+                  // TODO: Offer promotion, then calculate valid moves
+
+                }
+                  else{
+                    widget.bm.CalculateValidMoves();
+                  }
+              },
+
+              onWillAccept: (piece){
+                bool isValidMove = widget.bm.IsValidMove(piece, pos);
+                setState(() {
+                  this.hoverHighlight = pos;
+                  this.hoverWillAccept = isValidMove;
+                });
+                  return isValidMove;
+                },
+
+              onLeave: (piece){
+                setState(() {
+                  this.hoverHighlight = null;
+                  this.hoverWillAccept = null;
                 });
               },
+
               builder: (context, List<dynamic> candidateData,
                   List<dynamic> rejectedData) {
-                return img == null
-                    ? SizedBox(
-                        height: 0,
-                        width: 0,
-                      )
-                    : Draggable<Piece>(
-                        data: piece,
-                        //feedbackOffset: Offset.fromDirection(0, 50),
-                        feedback: SizedBox(
-                          child: img,
-                          width: 60,
-                          height: 60,
-                        ),
-                        childWhenDragging: Container(),
-                        child: FittedBox(fit: BoxFit.fill, child: img),
-                      );
+
+                  return Stack(
+                    children: [
+
+                      spaceContent,
+
+                      highlight
+
+                    ],
+                  );
+
+
               },
             );
           },
@@ -102,4 +157,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class Empty extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 0, width:  0);
+  }
+
 }
